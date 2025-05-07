b96dfb0842589f4774abbfc93189fde9f5d39492
KelvynLenis <kmartinslenis30@hotmail.com>
2025-05-07 14:31:44 -0300

    feat: setup API config

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -11,7 +11,14 @@ const SearchBar = ({ placeholder, onPress }: Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
       <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={'#ab8bff'} />
-      <TextInput onPress={onPress} placeholder={placeholder} value='' onChange={() => {}} placeholderTextColor={'#ab8b5db'} className='flex-1 ml-2 text-white' />
+      <TextInput 
+        onPress={onPress} 
+        placeholder={placeholder} 
+        value='' 
+        onChange={() => {}} 
+        placeholderTextColor={'#ab8b5db'} 
+        className='flex-1 ml-2 text-white' 
+      />
     </View>
   )
 }
---
+++ services/api.ts
@@ -0,0 +1,27 @@
+export const TMDB_CONFIG = {
+  BASE_URL: 'https://api.themoviedb.org/3',
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_TOKEN_ACCESS,
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_TOKEN_ACCESS}`
+  }
+}
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoit = query 
+  ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
+  : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoit, {
+    method: 'GET',
+    headers: TMDB_CONFIG.headers
+  })
+
+  if(!response.ok) {
+    throw new Error('Failed to fetch movies', response.statusText)
+  }
+
+  const data = await response.json()
+
+  return data.results
+}

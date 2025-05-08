f85694ca650dd212a609283247dd363e5cf13743
KelvynLenis <kmartinslenis30@hotmail.com>
2025-05-08 10:19:51 -0300

    feat: appwrite setup

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/SearchBar'
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api'
+import { updateSearchCount } from '@/services/appwrite'
 import useFetch from '@/services/useFetch'
 import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
@@ -13,9 +14,14 @@ const search = () => {
   const { data: movies, loading, error, refetch: loadMovies, reset } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
   useEffect(() => {
+    
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        if (movies.length > 0 && movies?.[0]) {
+          updateSearchCount(searchQuery, movies[0])
+        }
       } else {
         reset();
       }
